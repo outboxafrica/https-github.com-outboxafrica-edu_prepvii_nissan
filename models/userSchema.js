43f0ff50622e130c47d2b1aaf18f773ea1b675d2
= <=>
2020-09-01 15:29:15 +0300

    user, Q&A Schema

---
+++ models/userSchema.js
@@ -0,0 +1,13 @@
+//connect through mongoose 
+
+//user schema - Users can create an account and log in.
+
+//declare a new mongoose instance
+//model user accounts
+
+//email/username
+//password
+
+//user forms
+//Question
+//Answer
