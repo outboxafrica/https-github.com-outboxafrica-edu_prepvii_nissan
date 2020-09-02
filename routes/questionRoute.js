a0e0e9c63597ef92fcffc47cd2d5e56734454a3a
= <=>
2020-09-02 12:39:32 +0300

    re-modified quesiton routes for post data

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,19 +1,46 @@
+//declaring node packages
 const express = require('express');
-const bodyParser = require('body-parser');
+
+//routing pages
 const router = express.Router();
+
+const Quest = require('../models/questionModel');
+
+
 // const app = express();
 
 //app.METHOD(PATH,HANDLER);
-router.get('/', (req, res) => {
-    res.send('Home page')
-})
+/*
+ *router.get('/', (req, res) => {
+ *    res.send('Home page')
+ *})
+ */
 
 router.get('/questions', (req, res) => {
     res.send('Questions page')
 })
 
-router.post('/questions/post', () => {
+router.post('/questions/post', (req, res) => {
     res.send('Posting question to database')
 })
 
-module.exports = router;
+
+//Saving input information and then routing to next page
+async function as(req, res) {
+
+    const myData = new Quest(req.body); //body = question. Depends on the name used to pass data from the form.
+    try {
+        await myData.save();
+        console.log("item has been saved to database")
+
+        res.redirect("/forum") //redirects user to view Qs & As on the 'forum'(views)
+
+
+    } catch (err) {
+        console.error(`There is an error - ${err}`);
+    }
+}
+
+router.post("/", as);
+
+// module.exports = router;
