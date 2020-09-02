22b0c3c61d9f76e127018ba2b68697a48b620abf
= <=>
2020-09-02 11:37:52 +0300

    questions schema exported

--- models/questionModel.js
+++ models/questionModel.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose');
 //question schema - User can post questions to server & view questions on correc troute.
 
 //declare a new mongoose instance
-const user = new mongoose.Schema({
+const question = new mongoose.Schema({
     question: {
         type: String,
         required: true
     },
     user: {
-        //associated user
+        //associated user (maybe identified in form of a userID?)
     }
 });
 
 //model questions (stored in database)
-
+const Que = mongoose.model('question')
 
 //user forms
 //Question
 
-//export
-exports.Schema =
+//export module
+module.exports = Que;
