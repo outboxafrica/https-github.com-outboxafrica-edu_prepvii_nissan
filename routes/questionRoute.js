07946c0d6fac48854c862fa0e18569bf62f6c84c
= <=>
2020-09-02 12:07:47 +0300

    updated routes with post & export

---
+++ routes/questionRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+// const app = express();
+
+//app.METHOD(PATH,HANDLER);
+router.get('/', (req, res) => {
+    res.send('Home page')
+})
+
+router.get('/questions', (req, res) => {
+    res.send('Questions page')
+})
+
+router.post('/questions/post', () => {
+
+})
