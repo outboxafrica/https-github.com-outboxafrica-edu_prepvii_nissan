15515deef30fdeded72d2c11ade44140e1f36bc3
Lemi-Agrey <agreylemi@gmail.com>
2020-09-02 23:59:03 +0300

    First project commit

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 //express package
 const express = require('express')
-
+const mongoose=require('mongoose')
+const bodyParser=require('body-parser');
+const morgan=require('morgan');
+const userRoute=require('./routes/userRoute')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello JSON!")
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb+srv://alemin:lubang@cluster0.iwsvs.mongodb.net/StoryTeller?retryWrites=true&w=majority')
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
 })
 
-app.listen(3000, () => {
-    console.log(`Server listening on port http://localhost:${3000}`);
-})
+app.use('/userRoute', userRoute);
+
+app.use((req, res, next)=>{
+    const error= new Error('Not found');
+    error.status=404;
+    next(error);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+
+const PORT=process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+});
