cc9b325ed4e1e2e1ce41087ff3ac37602d3af19b
= <=>
2020-09-02 10:38:00 +0300

    updated file names

---
+++ models/questionModel.js
@@ -0,0 +1,12 @@
+//connect through mongoose 
+
+//user schema - Users can create an account and log in.
+
+//declare a new mongoose instance
+//model user accounts
+
+//user forms
+//Question
+//Answer
+
+//export
