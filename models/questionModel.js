a0c6dac15397fa410c87c6a132f9fa48ef67ffe3
= <=>
2020-09-03 16:22:45 +0300

    for when user answers question. added to Q schema

--- models/questionModel.js
+++ models/questionModel.js
@@ -2,14 +2,14 @@
 const mongoose = require('mongoose');
 //question schema - User can post questions to server & view questions on correc route.
 
-
 //declare a new mongoose instance
 const questionSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     question: {
         type: String,
         required: true
-    }
+    }, //if a person responds to this question they will be recorded as the "questionAnswerer"
+    questionAnswerer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
 //model questions (stored in database)
