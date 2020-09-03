2f571f59efbe14bce25ebc2515a1dc1d19c70fc6
= <=>
2020-09-03 16:27:46 +0300

    modified questionRoute to be similar to userRoute

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,48 +1,104 @@
 //declaring node packages
 const express = require('express');
+const mongoose = require('mongoose');
+//const bcrypt = require('bcrypt');
 
 //routing pages
 const router = express.Router();
-
+//importing question schema from "questionModel"
 const Quest = require('../models/questionModel');
-
-
-// const app = express();
+//importing user from "userModel" schema 
+const User = require('../models/userModel');
 
 //app.METHOD(PATH,HANDLER);
-/*
- *router.get('/', (req, res) => {
- *    res.send('Home page')
- *})
- */
 
-router.get('/questions', (req, res) => {
-    res.send('Questions page')
+router.post('/signup', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Mail exists"
+                })
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            first_name: req.body.first_name,
+                            last_name: req.body.last_name,
+                            email: req.body.email,
+                            password: hash
+                        })
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'User created'
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                })
+                            })
+                    }
+                })
+
+            }
+        })
+})
+router.post('login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status[401].json({
+                        message: 'Auth failed'
+                    })
+                }
+                if (result) {
+                    return res.status(200).json({
+                        message: "Auth successful"
+                    })
+                }
+                res.status(401).json({
+                    message: 'Auth failed'
+                })
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+router.delete('/:userId', (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "User deleted"
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
-router.delete('/', (req, res) => {
-    const myData = new Quest();
-    myData.deleteOne();
-    res.send('Posting question to database')
-})
-
-
-//Saving input information and then routing to next page
-async function as(req, res) {
-
-    const myData = new Quest(req.body); //body = question. Depends on the name used to pass data from the form.
-    try {
-        await myData.save();
-        console.log("item has been saved to database")
-
-        res.redirect("/forum") //redirects user to view Qs & As on the 'forum'(views)
-
-
-    } catch (err) {
-        console.error(`There is an error - ${err}`);
-    }
-}
-
-router.post("/", as);
-
-// module.exports = router;
+module.exports = router
