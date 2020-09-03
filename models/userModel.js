bd45d27464fdb4034f9b4913dbb481b4e4b89ab3
Lemi-Agrey <agreylemi@gmail.com>
2020-09-03 23:52:52 +0300

    user questions

--- models/userModel.js
+++ models/userModel.js
@@ -6,7 +6,7 @@ const userSchema=mongoose.Schema({
     last_name: {type: String, required: true},
     email: {type: String, 
         required: true, 
-        unique: true,
+        //unique: true,
         match: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
     },
     password: {type: String, required: true}
