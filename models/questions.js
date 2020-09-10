a56c528b25d5f7ac3436d40172abcd7c92b8f414
Lemi-Agrey <agreylemi@gmail.com>
2020-09-10 21:33:24 +0300

    Question edit

--- models/questions.js
+++ models/questions.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 
 const questionSchema=mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required : true},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required : false},
     question: {type: String, required: true}
 
 });
