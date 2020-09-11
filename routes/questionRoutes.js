750572a857e26ab8d2d1deaf2ea388044b432c89
= <=>
2020-09-11 13:10:50 +0300

    changed authentications & refcatored code

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,116 +1,115 @@
-const express=require('express');
-const router=express.Router();
-const mongoose=require('mongoose');
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
 
-const Questions=require('../models/questions'); 
-const {userRegister, userLogin, userAuth, serlializeUser, checkRole}=require('../utils/config');
+const Questions = require('../models/questions');
+const { userRegister, userLogin, userAuth, serlializeUser, checkRole } = require('../utils/config');
 
 //const checkAuth=require('../middleware/passport')
 
 
 
-router.get('/', (req, res, next)=>{
+router.get('/', (req, res, next) => {
     Questions.find()
-            .exec()
-            .then(doc=>{
-                res.status(200).json(doc)
+        .exec()
+        .then(doc => {
+            res.status(200).json(doc)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
             })
-            .catch(err=>{
-                console.log(err);
-                res.status(500).json({
-                    error : err
-                })
-            })
+        })
 });
 
-router.post('/',  userAuth, checkRole('admin', 'user'), (req, res, next)=>{
-    const question= new Questions({
+router.post('/', userAuth, (req, res, next) => {
+    const question = new Questions({
         _id: mongoose.Types.ObjectId(),
         question: req.body.question,
         user: req.body.userId
     });
     question
-    .save()
-    .then(result=>{
-        console.log(result);
-        res.status(201).json(result);
-    })
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error: err
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json(result);
         })
-    })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
 
     res.status(201).json({
-        message : 'Question was posted',
-        question:question 
+        message: 'Question was posted',
+        question: question
 
     });
 
 });
 
-router.get('/:questionId',  (req, res, next)=>{
+router.get('/:questionId', (req, res, next) => {
     Questions.findById(req.params.questionId)
-    .exec()
-    .then(question=>{
-        res.status(200).json({
-            question:question,
-            request: {
-                type: "GET",
-                url: "http://localhost:3000/questionRoutes"
-            }
+        .exec()
+        .then(question => {
+            res.status(200).json({
+                question: question,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/questionRoutes"
+                }
+            })
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
         })
-    }).catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error:err
-        })
-    })
 })
-router.patch('/:questionId', userAuth, checkRole('admin', 'user'), (req, res, next)=>{
-    const id=req.params.questionId;
-    const updateOps={}
-    for(const ops of req.body){
-        updateOps[ops.propName]=ops.value;
+router.patch('/:questionId', userAuth, checkRole('admin', 'user'), (req, res, next) => {
+    const id = req.params.questionId;
+    const updateOps = {}
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
     }
-    Questions.update({_id:id}, {$set : updateOps})
-    .exec()
-    .then(result=>{
-        console.log(result);
-        res.status(200).json(result)
-    })
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            message: "Internal server error"
+    Questions.update({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result)
         })
-    })
-    
-})
-
-router.delete("/:questionId", userAuth, checkRole('admin'), (req, res, next)=>{
-    Questions.remove({_id : req.params.questionId})
-    .exec()
-    .then(result=>{
-        res.status(200).json({
-            message: "Questions deleted sucessfully",
-            request: {
-                type: "POST",
-                url: "http://localhost:3000/questionRoutes",
-                body: {userId: "ID", question: "String"}
-            }
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "Internal server error"
+            })
         })
-
-    })
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error : err
-        })
-    })
 
 })
 
-module.exports=router;
+router.delete("/:questionId", userAuth, checkRole('user'), (req, res, next) => {
+    Questions.remove({ _id: req.params.questionId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Questions deleted sucessfully",
+                request: {
+                    type: "POST",
+                    url: "http://localhost:3000/questionRoutes",
+                    body: { userId: "ID", question: "String" }
+                }
+            })
 
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+
+})
+
+module.exports = router;
