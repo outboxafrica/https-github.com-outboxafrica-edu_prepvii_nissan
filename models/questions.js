d48b2e335b6d8b36ed86f1a4b191ad37510e8b31
= <=>
2020-09-11 13:03:18 +0300

    user falses

--- models/questions.js
+++ models/questions.js
@@ -1,11 +1,10 @@
+const mongoose = require('mongoose');
 
-const mongoose=require('mongoose');
-
-const questionSchema=mongoose.Schema({
+const questionSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required : true},
-    question: {type: String, required: true}
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
+    question: { type: String, required: true }
 
 });
 
-module.exports=mongoose.model('Questions', questionSchema);
+module.exports = mongoose.model('Questions', questionSchema);
